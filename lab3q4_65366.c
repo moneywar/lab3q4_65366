cb894ddc1eef196ca590c1ebfcd6f85b3c9df7d3
moneywar <65050366@kmitl.ac.th>
2023-07-20 14:55:04 +0700

    push program

---
+++ lab3q4_65366.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <stdbool.h>
+
+int main() {
+	int sum,input,avg,count;
+	sum,input,avg = 0;
+	count = -1;
+	do {
+		count++;
+		sum += input;
+		printf("Enter number : ");
+		scanf("%d",&input);
+	}while (input > 0);
+	printf("Summation : %d\n",sum);
+	printf("Average : %.6f",sum*1.0/count);
+	return 0;
+}
+
+
